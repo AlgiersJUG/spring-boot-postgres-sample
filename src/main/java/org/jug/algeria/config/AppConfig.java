f7e73c20e253c634fca7b4d11ab011114132a5c5
bouadma.abderrazak@gmail.com <bouadma.abderrazak@gmail.com>
2015-02-04 10:09:45 +0100

    used spring-boot idiom concerning the application.properties

--- src/main/java/org/jug/algeria/config/AppConfig.java
+++ src/main/java/org/jug/algeria/config/AppConfig.java
@@ -2,10 +2,8 @@ package org.jug.algeria.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan
-@PropertySource(value = "classpath:/db/postgres.properties")
 public class AppConfig {
 }
